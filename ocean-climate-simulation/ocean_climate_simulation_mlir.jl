9ad710621c5d6ee5d26f44c095ca57fbf1e8475d
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-03-10 21:33:09 +0100

    Add some prints to show where we are

--- ocean-climate-simulation/ocean_climate_simulation_mlir.jl
+++ ocean-climate-simulation/ocean_climate_simulation_mlir.jl
@@ -3,7 +3,8 @@ using Oceananigans.Architectures: Architectures
 
 include("data_free_ocean_climate_model.jl")
 PROFILE[] = true
-# simulation = data_free_ocean_climate_simulation_init(Architectures.ReactantState())
+
+@info "Generating model..."
 model = data_free_ocean_climate_model_init(Architectures.ReactantState())
 
 GC.gc(true); GC.gc(false); GC.gc(true)
@@ -17,14 +18,16 @@ function loop!(model)
     return nothing
 end
 
+# Unoptimized HLO
+@info "Compiling unoptimised kernel..."
 unopt = @code_hlo optimize=false raise=true loop!(model)
 
-# Unoptimized HLO
 open("unopt_ocean_climate_simulation.mlir", "w") do io
     show(io, unopt)
 end
 
 # Optimized HLO
+@info "Compiling optimised kernel..."
 opt = @code_hlo optimize=:before_jit raise=true loop!(model)
 
 open("opt_ocean_climate_simulation.mlir", "w") do io
